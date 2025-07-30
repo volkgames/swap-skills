a9a3ead6d7e21b293cf8d3479be93381edace68f
Volk <rahalilouay00@gmail.com>
2025-07-30 19:11:11 +0100

    Add ThemeProvider to RootLayout for improved theming support
    - Integrated the ThemeProvider from next-themes to enable theme management across the application.
    - Wrapped the children components in the ThemeProvider to allow for dynamic theme switching based on user preferences.

---
+++ src/app/(main)/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function ErrorPage({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  const isAuthenticationError = error.message.includes(
+    "Authentication failed"
+  );
+
+  return (
+    <div className="container mx-auto py-12 min-h-screen space-y-8">
+      {isAuthenticationError ? (
+        <>
+          <h1 className="page-title">Oops! You Need to Be Logged In</h1>
+          <p className="text-lg">To access this page, please log in first.</p>
+
+          <Button asChild>
+            <Link href="/sign-in">Sign In</Link>
+          </Button>
+        </>
+      ) : (
+        <>
+          <h1 className="page-title">Oops! Something went wrong</h1>
+          <p className="text-lg">{error.message}</p>
+        </>
+      )}
+    </div>
+  );
+}
